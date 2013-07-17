ecf3617b6bcaa25044fc92dfddfcfae8b5bf2221
Paulo Schneider <paulo.schneider@gmail.com>
2013-07-17 15:56:39 +0100

    Converting integers to numbers, when converting lists to record structure.
    @alexmuller, @phss

--- collector/list_converter.py
+++ collector/list_converter.py
@@ -4,8 +4,15 @@ class ListConverter(object):
     def to_dict(self, data):
         header, rows = data[0], data[1:]
 
+        def maybe_convert_to_number(s):
+            try:
+                return int(s)
+            except (ValueError, TypeError):
+                return s
+
         def row_to_dict(row):
-            return dict(zip(header, row))
+            converted_row = map(maybe_convert_to_number, row)
+            return dict(zip(header, converted_row))
 
         return map(row_to_dict, rows)      
 
--- tests/collector/test_list_converter.py
+++ tests/collector/test_list_converter.py
@@ -12,10 +12,21 @@ class TestListConverter(unittest.TestCase):
             ['Value21', 'Value22', 'Value23'],
             ['Value31', 'Value32', 'Value33']
         ]
-        expected_json = [
+        expected_output = [
             { 'Column1': 'Value11', 'Column2': 'Value12', 'Column3': 'Value13'},
             { 'Column1': 'Value21', 'Column2': 'Value22', 'Column3': 'Value23'},
             { 'Column1': 'Value31', 'Column2': 'Value32', 'Column3': 'Value33'}
         ]
         
-        assert_that(converter.to_dict(data), is_(expected_json))
+        assert_that(converter.to_dict(data), is_(expected_output))
+
+    def test_it_converts_only_an_integer_to_a_number(self):
+        data = [
+            ['IntegerColumn', 'FloatColumn', 'StringColumn'],
+            ['301', '42.35', 'This is still a string']
+        ]
+        expected_output = [
+            { 'IntegerColumn': 301, 'FloatColumn': '42.35', 'StringColumn': 'This is still a string' }
+        ]
+
+        assert_that(ListConverter().to_dict(data), is_(expected_output))
