9ee29f6a701c43388567e2e1a88de8c8f681bf30
Yolina Sotirova <yolina.sotirova@digital.cabinet-office.gov.uk>
2013-07-22 15:06:51 +0000

    Adding jenkins script for backdrop-google-spreadsheet-collector.
    @YolinaS
    @gtrogers

---
+++ jenkins.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ -z "$JOB_NAME" ]]; then
+    echo -e "\033[31m\$JOB_NAME is not defined!\033[0m"
+    exit 128
+fi
+
+VIRTUALENV_DIR=/var/tmp/virtualenvs/$(echo ${JOB_NAME} | tr ' ' '-')
+PIP_DOWNLOAD_CACHE=/var/tmp/pip_download_cache
+
+basedir=$(dirname $0)
+
+# create empty virtualenv
+virtualenv --clear --no-site-packages $VIRTUALENV_DIR
+source $VIRTUALENV_DIR/bin/activate
+
+pip install -r requirements_for_tests.txt
+
+nosetests
