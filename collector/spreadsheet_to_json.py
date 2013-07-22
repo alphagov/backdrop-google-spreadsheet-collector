800746a419cdde16ca762348a959dcfc85c4f51d
Yolina <ysotirov@thoughtworks.com>
2013-07-22 15:06:17 +0100

    Fixed collector to read the config file correctly. Works with a modified google spreadsheet now.
    @YolinaS
    @phss

--- collector/spreadsheet_to_json.py
+++ collector/spreadsheet_to_json.py
@@ -11,9 +11,9 @@ def parse_args(args):
 def get_google_spreadsheet_data(username, password, key):
     google = gspread.login(username, password)
     spreadsheet = google.open_by_key(key)
-    
+
     return spreadsheet.sheet1.get_all_values()
-    
+
 def convert_to_records(data):
     """Transforms a list of lists into a list of dictionaries, where data[0]
        is the header row of data"""
@@ -29,13 +29,13 @@ def convert_to_records(data):
         converted_row = map(process_cell, row)
         return dict(zip(header, converted_row))
 
-    return map(row_to_dict, rows)      
+    return map(row_to_dict, rows)
 
 
 def spreadsheet_to_json(args):
     arguments = parse_args(args)
     config = json.loads(open("config.json").read())
-    this_config = config[arguments.file]
+    this_config = config[arguments.doc]
     raw_data = get_google_spreadsheet_data(this_config['username'],
                                            this_config['password'],
                                            this_config['key'])
